ca5222ad0f811a8e663a0bf3479f649771331a44
Bob Robey <brobey@lanl.gov>
2019-07-06 18:50:35 -0600

    Updating copyright to Triad

--- gpu.h
+++ gpu.h
@@ -1,16 +1,16 @@
-/* Copyright 2013-14.  Los Alamos National Security, LLC. This material was produced
- * under U.S. Government contract DE-AC52-06NA25396 for Los Alamos National 
- * Laboratory (LANL), which is operated by Los Alamos National Security, LLC
+/* Copyright 2013-19.  Triad National Security, LLC. This material was produced
+ * under U.S. Government contract 89233218CNA000001 for Los Alamos National 
+ * Laboratory (LANL), which is operated by Triad National Security, LLC
  * for the U.S. Department of Energy. The U.S. Government has rights to use,
- * reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR LOS
- * ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR
+ * TRIAD NATIONAL SECURITY, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
  * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is modified
  * to produce derivative works, such modified software should be clearly marked,
  * so as not to confuse it with the version available from LANL.   
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
- * of the License at
+ * of the License at 
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -20,7 +20,7 @@
  * specific language governing permissions and limitations under the License.
  *
  * Under this license, it is required to include a reference to this work. We
- * request that each derivative work contain a reference to LANL Copyright
+ * request that each derivative work contain a reference to LANL Copyright 
  * Disclosure C14043/LA-CC-14-003 so that this work's impact can be roughly
  * measured.
  *
@@ -28,10 +28,10 @@
  */
 
 /*
- *  Authors: Bob Robey       XCP-2   brobey@lanl.gov
- *           Peter Ahrens            peter.ahrens@lanl.gov, ptrahrens@gmail.com
- *           Sara Hartse             sara@lanl.gov, sara.hartse@gmail.com
- *           Rebecka Tumblin         rtumblin@lanl.gov, rebeckatumblin@gmail.com
+ *  Authors: Bob Robey         XCP-2   brobey@lanl.gov
+ *           Peter Ahrens      XCP-2   peter.ahrens@lanl.gov, ptrahrens@gmail.com
+ *           Sara Hartse       XCP-2   sara@lanl.gov, 
+ *           Rebecka Tumblin           rtumblin@lanl.gov, rebeckatumblin@gmail.com
  */
 
 /* Modified from LANL Copyright Disclosure C13002/LA-CC-12-022 */
